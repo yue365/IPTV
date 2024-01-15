08615d72c88c7186b9b89967c98af52da2b2723f
yue365 <59732304+yue365@users.noreply.github.com>
2024-01-15 20:18:11 +0800

    Update and rename Main.py to IPTV_Check.py

--- IPTV_Check.py
+++ IPTV_Check.py
@@ -1,20 +1,15 @@
-import csv
-import sys
-import json
+# -*- coding: UTF-8 -*-
 import os
 import re
-import traceback
-import requests
-import subprocess
+import json
 import time
 import shutil
-from ffmpy import FFprobe
-from subprocess import PIPE
-from sys import stdout
-from termcolor import colored, RESET
+import traceback
 from datetime import datetime
+from subprocess import PIPE
+
+from ffmpy import FFprobe
 from func_timeout import func_set_timeout, FunctionTimedOut
-from requests.adapters import HTTPAdapter
 
 dt = datetime.now()
 # Channel	Group	Source	Link    Description
@@ -30,78 +25,79 @@ uniqueList = []
 
 
 @func_set_timeout(18)
-def get_stream(num, clist, uri):
+def get_stream(num, csvRowArry, url):
     try:
         ffprobe = FFprobe(
-            inputs={uri: ' -v error -show_format -show_streams -print_format json'})
+            inputs={url: ' -v error -show_format -show_streams -print_format json'})
         cdata = json.loads(ffprobe.run(
             stdout=PIPE, stderr=PIPE)[0].decode('utf-8'))
         return cdata
     except Exception as e:
         # traceback.print_exc()
-        print('{},{},{}'.format('ffprobe返回异常', clist[0], clist[1]))
+        print('{},{},{}'.format('ffprobe返回异常', csvRowArry[0], csvRowArry[1]))
         return False
 
 
-def check_channel(csvRow, num):
-    # clist 为一行 csv
-    iptv_url = csvRow[1]
-    # requests.adapters.DEFAULT_RETRIES = 3
+def check_channel(csvRowArray, taskNum):
+    iptvUrl = csvRowArray[1]
     try:
-        video_metaData = get_stream(num, csvRow, iptv_url)
+        video_metaData = get_stream(taskNum, csvRowArray, iptvUrl)
         if video_metaData:
             flagAudio = 0
             flagVideo = 0
-            flagHEVC = 0
+            codec_name = ''
             flagHDR = 0
-            vheight = 0
-            vwidth = 0
+            width = 0
+            height = 0
+            bitRate = 2048
             frameRate = 25
             for i in video_metaData['streams']:
                 if i['codec_type'] == 'video':
                     flagVideo = 1
+                    codec_name = i['codec_name'].upper()
+                    # codec_name = codec_name.upper()
                     if 'color_space' in i:
                         # https://www.reddit.com/r/ffmpeg/comments/kjwxm9/how_to_detect_if_video_is_hdr_or_sdr_batch_script/
                         if 'bt2020' in i['color_space']:
                             flagHDR = 1
-                    if i['codec_name'] == 'hevc':
-                        flagHEVC = 1
-                    if vwidth <= i['coded_width']:  # 取最高分辨率
-                        vwidth = i['coded_width']
-                        vheight = i['coded_height']
+                    if 'coded_width' in i:  # 取最高分辨率
+                        width = i['coded_width']
+                        height = i['coded_height']
                     if i['avg_frame_rate'] == '25/1':
-                        frameRate = '25FPS';
+                        frameRate = '25fps'
                     if i['avg_frame_rate'] == '50/1':
-                        frameRate = '50FPS';
+                        frameRate = '50fps'
+                    if 'tags' in i:
+                        bitRate = i['tags']['variant_bitrate']
+                        if bitRate.isdigit():
+                           bitRate = int(bitRate) / width / height
+                           bitRate = round(bitRate, 2)
+                        else:
+                            bitRate = 2048
                 elif i['codec_type'] == 'audio':
                     flagAudio = 1
             if flagAudio == 0:
                 print('[{}] {}({}) Error: Video Only!'.format(
-                    str(num), csvRow[0], csvRow[1]))
+                    str(taskNum), csvRowArray[0], csvRowArray[1]))
                 return False
             if flagVideo == 0:
                 print('[{}] {}({}) Error: Audio Only!'.format(
-                    str(num), csvRow[0], csvRow[1]))
+                    str(taskNum), csvRowArray[0], csvRowArray[1]))
                 return False
-            if (vwidth == 0) or (vheight == 0):
+            if (width == 0) or (height == 0):
                 print('[{}] {}({}) Error: {}x{}'.format(
-                    str(num), csvRow[0], csvRow[1], vwidth, vheight))
+                    str(taskNum), csvRowArray[0], csvRowArray[1], width, height))
 
             if flagHDR == 0:
-                # 编号，名称，分辨率，帧率，url
-                print('[{}],{},{}*{},{},{}'.format(str(num), csvRow[0], vwidth, vheight, frameRate, csvRow[1]))
-                # print(csvRow[1])
-                return [vwidth, vheight, "H264", frameRate]
+                # 编号，分辨率，帧率，码率,编码格式，名称，url
+                print('[{}] {}*{},{},{},{},{},{}'.format(str(taskNum), width, height, codec_name, frameRate, bitRate,
+                                                         csvRowArray[0], csvRowArray[1]))
+                return [width, height, codec_name, frameRate, bitRate]
             if flagHDR == 1:
                 print(
-                    '[{}] {}({}),分辨率:{}*{},(HDR Enabled),帧率:{}'.format(str(num), csvRow[0], csvRow[1], vwidth, vheight,
-                                                                       frameRate))
-                return [vwidth, vheight, 'HDR', frameRate]
-            if flagHEVC == 1:  # https://news.ycombinator.com/item?id=19389496  默认有HDR的算HEVC
-                print(
-                    '[{}] {}({}),分辨率:{}*{},(HEVC Enabled),帧率:{}'.format(str(num), csvRow[0], csvRow[1], vwidth, vheight,
-                                                                        frameRate))
-                return [vwidth, vheight, 'HEVC', frameRate]
+                    '[{}] {}*{},{},{},(HDR),{},{}'.format(str(taskNum), width, height, frameRate, bitRate,
+                                                                  csvRowArray[0], csvRowArray[1]))
+                return [width, height, codec_name+'(HDR)', frameRate, bitRate]
         else:
             return False
 
@@ -109,14 +105,13 @@ def check_channel(csvRow, num):
         # 通过，写入
         with open('checked_error.txt', 'a+', encoding="utf-8") as f1:
             # 名称，URL
-            print('{},{}'.format(csvRow[0], csvRow[1]), file=f1)
+            print('{},{}'.format(csvRowArray[0], csvRowArray[1]), file=f1)
+            # traceback.print_exc()
         return False
 
 
 def print_info():
-    print('Time: {}-{}-{} {}:{}'.format(dt.year,
-                                        dt.month, dt.day, dt.hour, dt.minute))
-    # subprocess.run(['ffprobe'])
+    print('Time: {}-{}-{} {}:{}'.format(dt.year, dt.month, dt.day, dt.hour, dt.minute))
 
 
 def rm_files(target, selection):
@@ -156,27 +151,27 @@ def main():
     # read txt method three
     f2 = open("playlist.txt", "r", encoding="UTF-8")
     all_lines = f2.readlines()
-    num = 1;
+    num = 1
     for line3 in all_lines:
         # print line3
-        data_row = line3.replace("\n", "").replace("\r", "").split(",")
+        dataRow = line3.replace("\n", "").replace("\r", "").split(",")
         try:
-            if data_row[1] in uniqueList:
+            if dataRow[1] in uniqueList:
                 ret = False
             else:
-                uniqueList.append(data_row[1])
-                ret = check_channel(data_row, num)
+                uniqueList.append(dataRow[1])
+                ret = check_channel(dataRow, num)
         except FunctionTimedOut as e:
             # traceback.print_exc()
             print('[{}] {}({}) Error:{}'.format(
-                str(num), data_row[0], data_row[1], str(e)))
+                str(num), dataRow[0], dataRow[1], str(e)))
             ret = False
         if (ret):
             # 通过，写入
             with open('checked{}.txt'.format(times), 'a+', encoding="utf-8") as f1:
                 # 编号，名称，分辨率，(HDR|HEVC|H264),帧率，URL
-                print('{},{},{}*{},{},{},{}'.format(num, data_row[0], ret[0], ret[1], ret[2], ret[3],
-                                                  data_row[1]), file=f1)
+                print('[{}],{}x{},{},{},{},{},{}'.format(num, ret[0], ret[1], ret[2], ret[3], ret[4],
+                                                       dataRow[0],dataRow[1]), file=f1)
             total = total + 1
         num = num + 1
         time.sleep(0.25)
