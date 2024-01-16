2890d024b3b8fed98fa1ddeb910084b383d20828
yue365 <59732304+yue365@users.noreply.github.com>
2024-01-16 17:08:19 +0800

    Update IPTV_Check.py
    获取声音信息，以筛选优质直播源

--- IPTV_Check.py
+++ IPTV_Check.py
@@ -25,7 +25,7 @@ uniqueList = []
 
 
 @func_set_timeout(18)
-def get_stream(num, csvRowArry, url):
+def get_stream(num, playlist_row_arry, url):
     try:
         ffprobe = FFprobe(
             inputs={url: ' -v error -show_format -show_streams -print_format json'})
@@ -34,80 +34,103 @@ def get_stream(num, csvRowArry, url):
         return cdata
     except Exception as e:
         # traceback.print_exc()
-        print('{},{},{}'.format('ffprobe返回异常', csvRowArry[0], csvRowArry[1]))
+        # print('{},{},{}'.format('ffprobe返回异常', csvRowArry[0], csvRowArry[1]))
+        with open('checked_error.txt', 'a+', encoding="utf-8") as f1:
+            print('ffprobe返回异常,{},{}'.format(playlist_row_arry[0], playlist_row_arry[1]), file=f1)
         return False
 
 
-def check_channel(csvRowArray, taskNum):
-    iptvUrl = csvRowArray[1]
+def check_channel(playlist_row_arr, taskNum):
+    iptvUrl = playlist_row_arr[1]
     try:
-        video_metaData = get_stream(taskNum, csvRowArray, iptvUrl)
+        video_metaData = get_stream(taskNum, playlist_row_arr, iptvUrl)
         if video_metaData:
-            flagAudio = 0
-            flagVideo = 0
+            # 视频信息
+            hdr_flag = 0
+            audio_flag = 0
+            video_flag = 0
             codec_name = ''
-            flagHDR = 0
             width = 0
             height = 0
-            bitRate = 2048
-            frameRate = 25
-            for i in video_metaData['streams']:
-                if i['codec_type'] == 'video':
-                    flagVideo = 1
-                    codec_name = i['codec_name'].upper()
-                    # codec_name = codec_name.upper()
-                    if 'color_space' in i:
+            frame_rate = 25
+            video_bit_rate = 2048
+            # 音频信息
+            audio_bit_rate = ''
+            audio_sample_rate = ''
+            audio_channels = ''
+            channel_layout = 'stereo'
+            if len(video_metaData['streams']) > 0:
+                for i in video_metaData['streams']:
+                    if i['codec_type'] == 'video':
+                        video_flag = 1
+                        codec_name = i['codec_name'].upper()
                         # https://www.reddit.com/r/ffmpeg/comments/kjwxm9/how_to_detect_if_video_is_hdr_or_sdr_batch_script/
-                        if 'bt2020' in i['color_space']:
-                            flagHDR = 1
-                    if 'coded_width' in i:  # 取最高分辨率
-                        width = i['coded_width']
-                        height = i['coded_height']
-                    if i['avg_frame_rate'] == '25/1':
-                        frameRate = '25fps'
-                    if i['avg_frame_rate'] == '50/1':
-                        frameRate = '50fps'
-                    if 'tags' in i:
-                        bitRate = i['tags']['variant_bitrate']
-                        if bitRate.isdigit():
-                           bitRate = int(bitRate) / width / height
-                           bitRate = round(bitRate, 2)
-                        else:
-                            bitRate = 2048
-                elif i['codec_type'] == 'audio':
-                    flagAudio = 1
-            if flagAudio == 0:
+                        if 'color_space' in i:
+                            if 'bt2020' in i['color_space']:
+                                hdr_flag = 1
+                        if 'coded_width' in i:  # 取最高分辨率
+                            width = i['width']
+                            height = i['height']
+                        if i['avg_frame_rate'] == '25/1':
+                            frame_rate = '25fps'
+                        if i['avg_frame_rate'] == '50/1':
+                            frame_rate = '50fps'
+                        # if 'tags' in i:
+                        #     bitRate = i['tags']['variant_bitrate']
+                        #     if bitRate.isdigit():
+                        #         bitRate = int(bitRate) / width / height
+                        #         bitRate = round(bitRate, 2)
+                        #     else:
+                        #         bitRate = 2048
+                    if i['codec_type'] == 'audio':
+                        audio_flag = 1
+                        if 'channels' in i:
+                            audio_channels = i['channels']
+                        if 'channel_layout' in i:
+                            channel_layout = i['channel_layout']
+                        if 'bit_rate' in i:
+                            audio_bit_rate = i['bit_rate']
+                        if 'sample_rate' in i:
+                            audio_sample_rate = i['sample_rate']
+            if audio_flag == 0:
                 print('[{}] {}({}) Error: Video Only!'.format(
-                    str(taskNum), csvRowArray[0], csvRowArray[1]))
+                    taskNum, playlist_row_arr[0], playlist_row_arr[1]))
                 return False
-            if flagVideo == 0:
+            if video_flag == 0:
                 print('[{}] {}({}) Error: Audio Only!'.format(
-                    str(taskNum), csvRowArray[0], csvRowArray[1]))
+                    taskNum, playlist_row_arr[0], playlist_row_arr[1]))
                 return False
             if (width == 0) or (height == 0):
                 print('[{}] {}({}) Error: {}x{}'.format(
-                    str(taskNum), csvRowArray[0], csvRowArray[1], width, height))
+                    taskNum, playlist_row_arr[0], playlist_row_arr[1], width, height))
 
-            if flagHDR == 0:
-                # 编号，分辨率，帧率，码率,编码格式，名称，url
-                print('[{}] {}*{},{},{},{},{},{}'.format(str(taskNum), width, height, codec_name, frameRate, bitRate,
-                                                         csvRowArray[0], csvRowArray[1]))
-                return [width, height, codec_name, frameRate, bitRate]
-            if flagHDR == 1:
-                print(
-                    '[{}] {}*{},{},{},(HDR),{},{}'.format(str(taskNum), width, height, frameRate, bitRate,
-                                                                  csvRowArray[0], csvRowArray[1]))
-                return [width, height, codec_name+'(HDR)', frameRate, bitRate]
+            if hdr_flag == 0:
+                print('[{}],分辨率:{}*{},视频编码:{},帧率:{},比特率:{},声道数:{},声道类型:{},声音采样率:{},声音比特率:{},{},{}'.format(
+                    taskNum, width, height, codec_name, frame_rate, video_bit_rate,
+                    audio_channels, channel_layout, audio_sample_rate, audio_bit_rate,
+                    playlist_row_arr[0], playlist_row_arr[1]))
+
+                return [width, height, codec_name, frame_rate, video_bit_rate, audio_channels,
+                        channel_layout, audio_sample_rate, audio_bit_rate]
+            if hdr_flag == 1:
+                codec_name = codec_name + '(HDR)'
+                print('[{}],分辨率:{}*{},视频编码:{},帧率:{},比特率:{},声道数:{},声道类型:{},声音采样率:{},声音比特率:{},{},{}'.format(
+                    taskNum, width, height, codec_name, frame_rate, video_bit_rate, audio_channels, channel_layout,
+                    audio_sample_rate, audio_bit_rate, playlist_row_arr[0], playlist_row_arr[1]))
+
+                return [width, height, codec_name, frame_rate, video_bit_rate, audio_channels,
+                        channel_layout, audio_sample_rate, audio_bit_rate]
+            else:
+                return False
         else:
             return False
-
     except Exception as e:
         # 通过，写入
-        with open('checked_error.txt', 'a+', encoding="utf-8") as f1:
-            # 名称，URL
-            print('{},{}'.format(csvRowArray[0], csvRowArray[1]), file=f1)
-            # traceback.print_exc()
-        return False
+        # with open('checked_error.txt', 'a+', encoding="utf-8") as f1:
+        #     # 名称，URL
+        #     print('{},{}'.format(csvRowArray[0], csvRowArray[1]), file=f1)
+        traceback.print_exc()
+    return False
 
 
 def print_info():
@@ -157,21 +180,22 @@ def main():
         dataRow = line3.replace("\n", "").replace("\r", "").split(",")
         try:
             if dataRow[1] in uniqueList:
-                ret = False
+                res = False
             else:
                 uniqueList.append(dataRow[1])
-                ret = check_channel(dataRow, num)
+                res = check_channel(dataRow, num)
         except FunctionTimedOut as e:
             # traceback.print_exc()
             print('[{}] {}({}) Error:{}'.format(
                 str(num), dataRow[0], dataRow[1], str(e)))
-            ret = False
-        if (ret):
+            res = False
+        if (res):
             # 通过，写入
             with open('checked{}.txt'.format(times), 'a+', encoding="utf-8") as f1:
-                # 编号，名称，分辨率，(HDR|HEVC|H264),帧率，URL
-                print('[{}],{}x{},{},{},{},{},{}'.format(num, ret[0], ret[1], ret[2], ret[3], ret[4],
-                                                       dataRow[0],dataRow[1]), file=f1)
+                # 编号，名称，分辨率，(HDR|HEVC|H264),帧率，声道数，声道类型，声音采样率，声音比特率，URL
+                print('[{}],分辨率:{}*{},视频编码:{},帧率:{},比特率:{},声道数:{},声道类型:{},声音采样率:{},声音比特率:{},{},{}'.format(
+                    num, res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8],
+                    dataRow[0], dataRow[1]), file=f1)
             total = total + 1
         num = num + 1
         time.sleep(0.25)
